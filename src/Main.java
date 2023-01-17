96997de7140c68bfa2617231cbc6e40c10f240ab
Evgeniy <volhin.z@mail.ru>
2023-01-17 13:40:02 +0300

    fdfhgj

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("Hello worjhjld!");
         System.out.println("Hello world!");
     }
 }
